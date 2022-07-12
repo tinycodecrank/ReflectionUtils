7b0657b199bad4759bf52e93ff484bae17f8e554
tinycodecrank <tinycodecrank@gmail.com>
2022-07-12 11:36:08 +0200

    initial commit

---
+++ src/de/tinycodecrank/reflectionUtils/Accessor.java
@@ -0,0 +1,80 @@
+package de.tinycodecrank.reflectionUtils;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Member;
+import java.lang.reflect.Modifier;
+
+public final class Accessor<E extends AccessibleObject & Member>
+{
+	private final Object	parent;
+	private final E			element;
+	
+	public Accessor(Object parent, E element)
+	{
+		this.parent		= parent;
+		this.element	= element;
+	}
+	
+	public Accessor(E element)
+	{
+		this(null, element);
+	}
+	
+	public <Value> void perform(AccessConsumer<E, Object, Value> action, Value value)
+		throws IllegalArgumentException,
+		IllegalAccessException
+	{
+		final boolean isAccessible = makeAccessible();
+		try
+		{
+			action.accept(element, parent, value);
+		}
+		finally
+		{
+			revertAccess(isAccessible);
+		}
+	}
+	
+	public <Return> Return perform(AccessSupplier<E, Object, Return> action)
+		throws IllegalArgumentException,
+		IllegalAccessException
+	{
+		final boolean isAccessible = makeAccessible();
+		try
+		{
+			return action.get(element, parent);
+		}
+		finally
+		{
+			revertAccess(isAccessible);
+		}
+	}
+	
+	private boolean makeAccessible()
+	{
+		final boolean isAccessible;
+		if (Modifier.isStatic(element.getModifiers()))
+			isAccessible = element.canAccess(null);
+		else
+			isAccessible = element.canAccess(parent);
+		element.setAccessible(true);
+		return isAccessible;
+	}
+	
+	private void revertAccess(boolean previous)
+	{
+		element.setAccessible(previous);
+	}
+	
+	@FunctionalInterface
+	public static interface AccessConsumer<Parent, AccObj, Value>
+	{
+		void accept(Parent parent, AccObj member, Value value) throws IllegalArgumentException, IllegalAccessException;
+	}
+	
+	@FunctionalInterface
+	public static interface AccessSupplier<Parent, AccObj, Return>
+	{
+		Return get(Parent parent, AccObj member) throws IllegalArgumentException, IllegalAccessException;
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,4 @@
+module de.tinycodecrank.ReflectionUtils
+{
+	exports de.tinycodecrank.reflectionUtils;
+}
